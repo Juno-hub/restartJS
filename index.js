38bfd1c8e2ae81b3ba4a5de035fbdb96cc76df76
Bae Junho <58718078+Juno-hub@users.noreply.github.com>
2021-01-13 13:46:41 +0900

    if part 2

--- index.js
+++ index.js
@@ -4,20 +4,22 @@
 
 const title = document.querySelector("#title");
 
-const BASE_COLOR = "rgb(52, 73, 94)";
-const OTHER_COLOR = "#7f8fa6";
+const CLICKED_CLASS = "clicked";
+
+// function handleClick(){
+//     const hasClass = title.classList.contains(CLICKED_CLASS);
+//     if (hasClass) {
+//         title.classList.remove(CLICKED_CLASS);
+//     } else {
+//         title.classList.add(CLICKED_CLASS);
+//     }
+// }
 
 function handleClick(){
-    const currentColor = title.style.color;
-    if (currentColor === BASE_COLOR) {
-        title.style.color = OTHER_COLOR;
-    } else {
-        title.style.color = BASE_COLOR;
-    }
+   title.classList.toggle(CLICKED_CLASS);
 }
 
 function init(){
-    title.style.color = BASE_COLOR;
     title.addEventListener("click", handleClick)
 }
 init();
